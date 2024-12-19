16148f00a7070d8e848179514e61f68643df60a8
Rapeephat01 <watdee00@GMAIL.COM>
2024-12-19 21:36:33 +0700

    Update lab10_2.cpp

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -3,16 +3,35 @@
 #include<string>
 using namespace std;
 
-int main (){
-	ifstream source;
-	ofstream dest;
-	source.open("cheerbook.txt") ;
-	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+int main() {
+    ifstream source;
+    ofstream dest;
+    source.open("cheerbook.txt");
+    dest.open("cheerbook_copy.txt");
+
+    if (!source) {
+        cerr << "Error: Unable to open cheerbook.txt" << endl;
+        return 1;
+    }
+
+    if (!dest) {
+        cerr << "Error: Unable to open cheerbook_copy.txt" << endl;
+        return 1;
+    }
+
+    // Write header line
+    dest << "-------------------- BOOM ---------------------" << endl;
+
+    // Copy content from source to destination
+    string line;
+    while (getline(source, line)) {
+        dest << line << endl;
+    }
+
+    // Write footer line
+    dest << "-------------------- HA!! ---------------------" << endl;
 
     source.close();
     dest.close();
-	return 0;
+    return 0;
 }
